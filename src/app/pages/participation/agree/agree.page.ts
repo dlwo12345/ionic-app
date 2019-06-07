4994d64026a897665258ca79cb2d1edeb323f8f2
jaehong.lee <jaehong.lee@solarconnect.kr>
2019-06-07 18:44:16 +0900

    약관동의 비활성 추가

--- src/app/pages/participation/agree/agree.page.ts
+++ src/app/pages/participation/agree/agree.page.ts
@@ -1,27 +1,53 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
-import {Router, ActivatedRoute, NavigationEnd} from '@angular/router';
 import {Subscription} from 'rxjs';
 import {NavController} from '@ionic/angular';
+import {FormGroup, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-agree',
   templateUrl: './agree.page.html',
   styleUrls: ['./agree.page.scss']
 })
-export class AgreePage implements OnInit, OnDestroy {
-  routerSub: Subscription;
-  interval = null;
-  constructor(public navC: NavController) {}
+export class AgreePage implements OnInit {
+  agreeForm: FormGroup;
+  constructor(public navC: NavController, private formBuilder: FormBuilder) {
+    this.createForm();
+  }
 
   ngOnInit() {}
 
-  agree() {
-    this.navC.navigateForward('/participation/step1');
+  createForm(formData?: any) {
+    this.agreeForm = this.formBuilder.group({
+      agree1: true,
+      agree2: true
+    });
   }
 
-  ngOnDestroy(): void {
-    if (this.routerSub) {
-      this.routerSub.unsubscribe();
+  /**
+   * 모든 약관 동의여부 체크
+   * 모두 동의o -> return true
+   * 모두 동의x -> return false
+   */
+  agreeCheck() {
+    // form data list
+    const target = this.agreeForm.controls;
+    for (const i in target) {
+      if (target.hasOwnProperty(i)) {
+        const value = target[i].value; // 개별 아이템 value
+        if (!value) {
+          // false 값이 있으면 false 반환
+          return false;
+        }
+      }
     }
+    return true;
+  }
+
+  next() {
+    if (!this.agreeCheck()) {
+      alert('미동의 약관이 있습니다');
+    }
+
+    this.navC.navigateForward('/participation/step1');
   }
 }
