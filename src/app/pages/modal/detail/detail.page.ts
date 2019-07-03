3c5a0344dfa0453f461158e86fad39eb260df2d1
jaehong.lee <jaehong.lee@solarconnect.kr>
2019-07-03 19:07:05 +0900

    차트 추가

--- src/app/pages/modal/detail/detail.page.ts
+++ src/app/pages/modal/detail/detail.page.ts
@@ -1,29 +1,100 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  NgZone,
+  AfterViewInit,
+  OnDestroy
+} from '@angular/core';
 import * as moment from 'moment';
 import {NavParams, ModalController} from '@ionic/angular';
 import {MedalPage} from '../medal/medal.page';
 
-declare const Kakao: any;
-declare const ShareBand: any;
+import * as am4core from '@amcharts/amcharts4/core';
+import * as am4charts from '@amcharts/amcharts4/charts';
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+
+am4core.useTheme(am4themes_animated);
+
+declare const Kakao: any; // 카카오 API SDK
+declare const ShareBand: any; // 밴드
 
 @Component({
   selector: 'app-detail',
   templateUrl: 'detail.page.html',
   styleUrls: ['detail.page.scss']
 })
-export class DetailPage implements OnInit {
+export class DetailPage implements OnInit, AfterViewInit, OnDestroy {
   // "value" passed in componentProps
   @Input() value: number;
   premiumAuth = false; // SPEC, 수익금, 시공사 표시여부
+  private chart: am4charts.XYChart;
 
-  constructor(private navParams: NavParams, private modalC: ModalController) {
+  constructor(
+    private navParams: NavParams,
+    private modalC: ModalController,
+    private zone: NgZone
+  ) {
     // componentProps can also be accessed at construction time using NavParams
     console.log('navParams', navParams);
+  }
+
+  ngOnDestroy() {
+    this.zone.runOutsideAngular(() => {
+      if (this.chart) {
+        this.chart.dispose();
+      }
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.zone.runOutsideAngular(() => {
+      // Create chart instance
+      let chart = am4core.create('chartdiv', am4charts.XYChart);
+
+      // Add data
+      chart.data = [
+        {country: '1-1', visits: 2025},
+        {country: '1-2', visits: 1882},
+        {country: '1-3', visits: 1809},
+        {country: '1-4', visits: 1322},
+        {country: '1-5', visits: 1122},
+        {country: '1-6', visits: 1114},
+        {country: '1-7', visits: 984},
+        {country: '1-8', visits: 711},
+        {country: '1-9', visits: 665},
+        {country: '1-10', visits: 580},
+        {country: '1-11', visits: 443},
+        {country: '1-12', visits: 441},
+        {country: '1-13', visits: 395}
+      ];
+
+      // Create axes
+      let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+      categoryAxis.dataFields.category = 'country';
+      categoryAxis.renderer.grid.template.location = 0;
+      categoryAxis.renderer.minGridDistance = 30;
+
+      let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+      // Create series
+      let series = chart.series.push(new am4charts.ColumnSeries());
+      series.dataFields.valueY = 'visits';
+      series.dataFields.categoryX = 'country';
+      series.name = 'Visits';
+      series.columns.template.tooltipText = '{categoryX}: [bold]{valueY}[/]';
+      series.columns.template.fillOpacity = 0.8;
+
+      let columnTemplate = series.columns.template;
+      columnTemplate.strokeWidth = 2;
+      columnTemplate.strokeOpacity = 1;
+    });
   }
 
   ngOnInit() {
     this.shareKakao();
   }
+
   async closeModal() {
     const onClosedData = {};
     await this.modalC.dismiss(onClosedData);
