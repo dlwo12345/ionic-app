b841b156fadccfce4cb428251ae08035c435ffda
jaehong.lee <jaehong.lee@solarconnect.kr>
2019-07-03 11:35:20 +0900

    링크변경

--- src/app/pages/modal/detail/detail.page.ts
+++ src/app/pages/modal/detail/detail.page.ts
@@ -55,12 +55,13 @@ export class DetailPage implements OnInit {
       installTalk: true,
       content: {
         title: '홍길동님의 발전소',
-        description: '이번달엔 이렇게 발전이 되었습니다~~~~~~~',
+        description:
+          '이번달엔 이렇게 발전이 되었습니다~~~~~~~이번달엔 이렇게 발전이 되었습니다~~~~~~~',
         imageUrl:
           'http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
         link: {
-          mobileWebUrl: 'http://192.168.2.202:8100/tabs/rank',
-          webUrl: 'http://192.168.2.202:8100/tabs/rank'
+          mobileWebUrl: 'http://192.168.2.202:8100/tabs/my',
+          webUrl: 'http://192.168.2.202:8100/tabs/my'
         }
       },
       buttons: [
@@ -88,8 +89,10 @@ export class DetailPage implements OnInit {
     }
 
     // 웹공유방식;
-    content = 'https://map.naver.com/?pinId=18821453&dlevel=11&enc=b64&pinType=site&y=4b5d9325eb8986da16391a0924e22e86&x=bb84cb4ba4c2740d594884d572586d21&spi_ref=m_map_band';
-    const shareUrl = 'http://www.band.us/plugin/share?body=' + encodeURIComponent(content);
+    content =
+      'https://map.naver.com/?pinId=18821453&dlevel=11&enc=b64&pinType=site&y=4b5d9325eb8986da16391a0924e22e86&x=bb84cb4ba4c2740d594884d572586d21&spi_ref=m_map_band';
+    const shareUrl =
+      'http://www.band.us/plugin/share?body=' + encodeURIComponent(content);
     window.open(shareUrl, 'ShareBand', 'width=410, height=540, resizable=no');
   }
 }
